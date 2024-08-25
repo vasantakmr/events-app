dd76bd92aba99a41f9912d9262c9b50dcbcb85c1
Vasanta Kumar <vasantakmr6@gmail.com>
2024-08-26 00:56:47 +0530

    cloudinary

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { v2 as cloudinary } from "cloudinary";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export { cloudinary };
+
