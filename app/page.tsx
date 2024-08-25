3c2a88d3d4e5976e48b3a2c8ed640c8382e7cd09
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-08-25 23:50:05 +0530

    retrigger

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
   });
   return (
     <div className="max-w-4xl mx-auto p-6 sm:p-10">
-      <div className="flex flex-col gap-8">Homeee Page {event?.name}</div>
+      <div className="flex flex-col gap-8">Home Page {event?.name}</div>
     </div>
   );
 }
