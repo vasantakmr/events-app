e781f7f9e0771c242c4c7e55cbb6026ebbbc8bc4
Vasanta Kumar <vasantakmr6@gmail.com>
2024-09-06 19:11:17 +0530

    changed the dropdown options

--- components/auth/signin-button.tsx
+++ components/auth/signin-button.tsx
@@ -12,14 +12,18 @@ import {
 import { CircleUser } from "lucide-react";
 import { SignOut } from "./signout-button";
 import Image from "next/image";
+import Link from "next/link";
 
 export async function SignIn() {
   const session = await auth();
   const imageUrl = session?.user?.image || "/events/blur.webp";
   let userName;
   /* // TODO: name can be only first name and the first name can be trimmed*/
-  if(session?.user?.name) {
-    userName = (session.user.name.length > 15) ? session.user.name.substring(0, 13)+"..." :  session.user.name;
+  if (session?.user?.name) {
+    userName =
+      session.user.name.length > 15
+        ? session.user.name.substring(0, 13) + "..."
+        : session.user.name;
   }
 
   if (!session?.user) {
@@ -39,16 +43,23 @@ export async function SignIn() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="secondary" size="icon" className="rounded-full">
-          
           <CircleUser className="h-5 w-5" />
-          <Image alt="Profile Pic" src={imageUrl} width={100} height={100} className="rounded-full object-cover"/>
+          <Image
+            alt="Profile Pic"
+            src={imageUrl}
+            width={100}
+            height={100}
+            className="rounded-full object-cover"
+          />
           <span className="sr-only">Toggle user menu</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>{userName}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <Link href="/my-events">
+          <DropdownMenuItem>My Events</DropdownMenuItem>
+        </Link>
         <DropdownMenuItem>Support</DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
