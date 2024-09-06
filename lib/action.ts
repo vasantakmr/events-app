a70d86c79af442f5c49d1481590e3a5cee920619
Vasanta Kumar <vasantakmr6@gmail.com>
2024-09-06 17:55:24 +0530

    added cache

--- lib/action.ts
+++ lib/action.ts
@@ -1,26 +1,45 @@
+import { unstable_cache } from "next/cache";
 import { prisma } from "./prisma";
 
 export async function getEvent(eventId: string) {
-  try {
-    const event = await prisma.event.findUnique({
-      where: { id: eventId },
-    });
-    return event;
-  } catch (error) {
-    console.log("Failed to fetch event: ", error);
-  }
+  return await unstable_cache(
+    async () => {
+      try {
+        const event = await prisma.event.findUnique({
+          where: { id: eventId },
+        });
+        return event;
+      } catch (error) {
+        console.log("Failed to fetch event: " + eventId, error);
+      }
+    },
+    [`${eventId}-event`],
+    {
+      revalidate: 900,
+      tags: [`${eventId}-event`],
+    }
+  )();
 }
 
-export const getEvents = async () => {
-  try {
-    const events = await prisma.event.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return events;
-  } catch (error) {
-    console.error("Error fetching the events:", error);
-    return null;
-  }
-};
+export async function getEvents() {
+  return await unstable_cache(
+    async () => {
+      try {
+        const events = await prisma.event.findMany({
+          orderBy: {
+            createdAt: "desc",
+          },
+        });
+        return events;
+      } catch (error) {
+        console.error("Error fetching the events:", error);
+        return null;
+      }
+    },
+    [`allevents`],
+    {
+      revalidate: 900,
+      tags: [`allevents`],
+    }
+  )();
+}
